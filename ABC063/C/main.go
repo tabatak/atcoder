05a52990334fabc6a5c148bd5083efb541216681
tabatak <hamamoto.kazuto@gmail.com>
2020-03-11 21:35:25 +0900

    ABC063-C (自力AC)
    標準エラーに出力し続けてWAになってた。。

--- ABC063/C/main.go
+++ ABC063/C/main.go
@@ -1,32 +1,42 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math"
+	"os"
 )
 
 func main() {
+	r := bufio.NewReader(os.Stdin)
 	var n int
-	fmt.Scan(&n)
+	fmt.Fscan(r, &n)
 	s := make([]int, n)
 	for i := 0; i < n; i++ {
-		fmt.Scan(&s[i])
+		fmt.Fscan(r, &s[i])
 	}
 
-	min := math.MaxInt32
+	min := math.MaxInt64
 	sum := 0
 	for i := 0; i < n; i++ {
 		sum += s[i]
 		if s[i]%10 != 0 {
-			min = int(math.Min(float64(min), float64(s[i])))
+			min = minInt(min, s[i])
 		}
 	}
 
 	if sum%10 != 0 {
-		println(sum)
-	} else if min == math.MaxInt32 {
-		println(0)
+		fmt.Println(sum)
+	} else if min == math.MaxInt64 {
+		fmt.Println(0)
 	} else {
-		println(sum - min)
+		fmt.Println(sum - min)
 	}
+}
+
+func minInt(x int, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
