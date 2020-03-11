1865cae6e734f3ec984a9c3b4a3dd2ec4b4753e1
tabatak <hamamoto.kazuto@gmail.com>
2020-03-11 21:40:42 +0900

    関数置き場を追加

---
+++ template/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Fscan(r, &n)
+	s := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &s[i])
+	}
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
