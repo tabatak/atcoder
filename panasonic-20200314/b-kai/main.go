28f831f8fb544cb0cdcaee92938d4ba363ce9b8a
tabatak <hamamoto.kazuto@gmail.com>
2020-03-15 08:39:32 +0900

    Panasonic-20200314-B (解説後に整理)
    一発目でこれを書けるようになりたい。おおよそマスの半分が埋まりそうなのは見た目でわかるわけだし。。

---
+++ panasonic-20200314/b-kai/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var h, w int
+	fmt.Fscan(r, &h)
+	fmt.Fscan(r, &w)
+
+	if min(h, w) == 1 {
+		fmt.Println(1)
+		return
+	}
+
+	n := h * w
+	ans := (n + 1) / 2
+	fmt.Println(ans)
+}
+
+// Union-Find
+type unionFind struct {
+	d []int
+}
+
+func newUnionFind(n int) *unionFind {
+	uf := new(unionFind)
+	uf.d = make([]int, n)
+	for i := 0; i < n; i++ {
+		uf.d[i] = -1
+	}
+	return uf
+}
+
+func (uf *unionFind) find(x int) int {
+	if uf.d[x] < 0 {
+		return x
+	}
+	uf.d[x] = uf.find(uf.d[x])
+	return uf.d[x]
+}
+
+func (uf *unionFind) unite(x, y int) bool {
+	rootX := uf.find(x)
+	rootY := uf.find(y)
+	if rootX == rootY {
+		return false
+	}
+
+	if uf.d[rootX] < uf.d[rootY] {
+		uf.d[rootX] += uf.d[rootY]
+		uf.d[rootY] = rootX
+	} else {
+		uf.d[rootY] += uf.d[rootX]
+		uf.d[rootX] = rootY
+	}
+
+	return true
+}
+
+func (uf *unionFind) same(x, y int) bool {
+	return uf.find(x) == uf.find(y)
+}
+
+func (uf *unionFind) size(x int) int {
+	return -uf.d[uf.find(x)]
+}
+
+// Utility
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
