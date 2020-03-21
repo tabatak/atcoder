5906745bcca603a142e6bba6b677e643e60ffac5
tabatak <hamamoto.kazuto@gmail.com>
2020-03-21 18:06:45 +0900

    ABC058-C (自力AC)
    これもそのまま実装。

---
+++ abc058/c/main.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Fscan(r, &n)
+	ss := make([]string, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &ss[i])
+	}
+
+	alphabetCounts := make([]int, 26)
+	for i := 0; i < len(alphabetCounts); i++ {
+		alphabetCounts[i] = 50
+	}
+	for _, s := range ss {
+		tmpCounts := make([]int, 26)
+		for j := 0; j < len(s); j++ {
+			tmpCounts[s[j]-'a']++
+		}
+
+		for j := 0; j < 26; j++ {
+			alphabetCounts[j] = min(alphabetCounts[j], tmpCounts[j])
+		}
+	}
+
+	// 出力
+	w := bufio.NewWriter(os.Stdout)
+	defer w.Flush()
+	for i := 0; i < len(alphabetCounts); i++ {
+		for j := 0; j < alphabetCounts[i]; j++ {
+			fmt.Fprint(w, string('a'+byte(i)))
+		}
+	}
+	fmt.Fprintln(w, "")
+}
+
+// Union-Find
+type unionFind struct {
+	d []int
+}
+
+func newUnionFind(n int) *unionFind {
+	uf := new(unionFind)
+	uf.d = make([]int, n)
+	for i := 0; i < n; i++ {
+		uf.d[i] = -1
+	}
+	return uf
+}
+
+func (uf *unionFind) find(x int) int {
+	if uf.d[x] < 0 {
+		return x
+	}
+	uf.d[x] = uf.find(uf.d[x])
+	return uf.d[x]
+}
+
+func (uf *unionFind) unite(x, y int) bool {
+	rootX := uf.find(x)
+	rootY := uf.find(y)
+	if rootX == rootY {
+		return false
+	}
+
+	if uf.d[rootX] < uf.d[rootY] {
+		uf.d[rootX] += uf.d[rootY]
+		uf.d[rootY] = rootX
+	} else {
+		uf.d[rootY] += uf.d[rootX]
+		uf.d[rootX] = rootY
+	}
+
+	return true
+}
+
+func (uf *unionFind) same(x, y int) bool {
+	return uf.find(x) == uf.find(y)
+}
+
+func (uf *unionFind) size(x int) int {
+	return -uf.d[uf.find(x)]
+}
+
+// mod combination
+func modpow(a, n, mod int) int {
+	res := 1
+	for n > 0 {
+		if n%2 != 0 {
+			res = res * a % mod
+		}
+		a = a * a % mod
+		n = n / 2
+	}
+	return res
+}
+
+func modcomb(n, a, mod int) int {
+	x := 1
+	y := 1
+	for i := 0; i < a; i++ {
+		x = x * (n - i)
+		x %= mod
+		y = y * (i + 1)
+		y %= mod
+	}
+	return x * modpow(y, mod-2, mod) % mod
+}
+
+func modfactorial(n, mod int) int {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result = (result * i) % mod
+	}
+	return result
+}
+
+// Utility
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func primeFactor(x int) map[int]int {
+	res := make(map[int]int)
+	for i := 2; i*i <= x; i++ {
+		for x%i == 0 {
+			res[i]++
+			x = x / i
+		}
+	}
+	if x != 1 {
+		res[x] = 1
+	}
+	return res
+}
+
+func divisor(x int) []int {
+	res := make([]int, 0)
+	for i := 1; i*i <= x; i++ {
+		if x%i == 0 {
+			res = append(res, i)
+			if i != x/i {
+				res = append(res, x/i)
+			}
+		}
+	}
+	return res
+}
+
+func gcd(x, y int) int {
+	if y == 0 {
+		return x
+	}
+	return gcd(y, x%y)
+}
+
+func lcm(x, y int) int {
+	return x / gcd(x, y) * y
+}
