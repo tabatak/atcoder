510f766a725e9b6dd8862a82267ccd2a006108df
tabatak <hamamoto.kazuto@gmail.com>
2020-04-04 12:06:32 +0900

    ABC148-E (解説AC)
    整数問題がんばっていこ

--- abc148/e/main.go
+++ abc148/e/main.go
@@ -11,116 +11,16 @@ func main() {
 	var n int
 	fmt.Fscan(r, &n)
 
-	wtr := bufio.NewWriter(os.Stdout)
-	fmt.Fprintf(wtr, "%d ", n)
-	wtr.Flush()
-}
-
-// Union-Find
-type unionFind struct {
-	d []int
-}
-
-func newUnionFind(n int) *unionFind {
-	uf := new(unionFind)
-	uf.d = make([]int, n)
-	for i := 0; i < n; i++ {
-		uf.d[i] = -1
-	}
-	return uf
-}
-
-func (uf *unionFind) find(x int) int {
-	if uf.d[x] < 0 {
-		return x
-	}
-	uf.d[x] = uf.find(uf.d[x])
-	return uf.d[x]
-}
-
-func (uf *unionFind) unite(x, y int) bool {
-	rootX := uf.find(x)
-	rootY := uf.find(y)
-	if rootX == rootY {
-		return false
+	if n%2 != 0 {
+		fmt.Println(0)
+		return
 	}
 
-	if uf.d[rootX] < uf.d[rootY] {
-		uf.d[rootX] += uf.d[rootY]
-		uf.d[rootY] = rootX
-	} else {
-		uf.d[rootY] += uf.d[rootX]
-		uf.d[rootX] = rootY
+	ans := 0
+	divisor := 5
+	for divisor <= n/2 {
+		ans += n / 2 / divisor
+		divisor *= 5
 	}
-
-	return true
-}
-
-func (uf *unionFind) same(x, y int) bool {
-	return uf.find(x) == uf.find(y)
-}
-
-func (uf *unionFind) size(x int) int {
-	return -uf.d[uf.find(x)]
-}
-
-// Utility
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-func max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
-}
-
-func abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
-}
-
-func primeFactor(x int) map[int]int {
-	res := make(map[int]int)
-	for i := 2; i*i <= x; i++ {
-		for x%i == 0 {
-			res[i]++
-			x = x / i
-		}
-	}
-	if x != 1 {
-		res[x] = 1
-	}
-	return res
-}
-
-func divisor(x int) []int {
-	res := make([]int, 0)
-	for i := 1; i*i <= x; i++ {
-		if x%i == 0 {
-			res = append(res, i)
-			if i != x/i {
-				res = append(res, x/i)
-			}
-		}
-	}
-	return res
-}
-
-func gcd(x, y int) int {
-	if y == 0 {
-		return x
-	}
-	return gcd(y, x%y)
-}
-
-func lcm(x, y int) int {
-	return x / gcd(x, y) * y
+	fmt.Println(ans)
 }
