ce6aa26411d97a9c1df2b4ed8a14d2e41d65b551
tabatak <hamamoto.kazuto@gmail.com>
2020-04-13 07:45:02 +0900

    ABC043-C (自力AC)
    平均で解答した方はたまたまだったのかな。証明ができない。

---
+++ abc043/c/main.go
@@ -0,0 +1,256 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Fscan(r, &n)
+	as := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &as[i])
+	}
+
+	ans := math.MaxInt64
+	for i := -100; i <= 100; i++ {
+		sum := 0
+		for j := 0; j < n; j++ {
+			m := as[j] - i
+			sum += m * m
+		}
+		ans = min(ans, sum)
+	}
+
+	fmt.Println(ans)
+}
+
+// func main() {
+// 	r := bufio.NewReader(os.Stdin)
+// 	var n int
+// 	fmt.Fscan(r, &n)
+// 	as := make([]int, n)
+// 	sum := 0
+// 	for i := 0; i < n; i++ {
+// 		fmt.Fscan(r, &as[i])
+// 		sum += as[i]
+// 	}
+
+// 	v1 := sum / n
+// 	v2 := sum/n + 1
+// 	ans1 := 0
+// 	ans2 := 0
+// 	for i := 0; i < n; i++ {
+// 		tmp1 := abs(as[i] - v1)
+// 		ans1 += tmp1 * tmp1
+
+// 		tmp2 := abs(as[i] - v2)
+// 		ans2 += tmp2 * tmp2
+// 	}
+
+// 	ans := min(ans1, ans2)
+// 	fmt.Println(ans)
+// }
+
+// Union-Find
+type unionFind struct {
+	d []int
+}
+
+func newUnionFind(n int) *unionFind {
+	uf := new(unionFind)
+	uf.d = make([]int, n)
+	for i := 0; i < n; i++ {
+		uf.d[i] = -1
+	}
+	return uf
+}
+
+func (uf *unionFind) find(x int) int {
+	if uf.d[x] < 0 {
+		return x
+	}
+	uf.d[x] = uf.find(uf.d[x])
+	return uf.d[x]
+}
+
+func (uf *unionFind) unite(x, y int) bool {
+	rootX := uf.find(x)
+	rootY := uf.find(y)
+	if rootX == rootY {
+		return false
+	}
+
+	if uf.d[rootX] < uf.d[rootY] {
+		uf.d[rootX] += uf.d[rootY]
+		uf.d[rootY] = rootX
+	} else {
+		uf.d[rootY] += uf.d[rootX]
+		uf.d[rootX] = rootY
+	}
+
+	return true
+}
+
+func (uf *unionFind) same(x, y int) bool {
+	return uf.find(x) == uf.find(y)
+}
+
+func (uf *unionFind) size(x int) int {
+	return -uf.d[uf.find(x)]
+}
+
+// mod combination
+func modpow(a, n, mod int) int {
+	res := 1
+	for n > 0 {
+		if n%2 != 0 {
+			res = res * a % mod
+		}
+		a = a * a % mod
+		n = n / 2
+	}
+	return res
+}
+
+func modcomb(n, a, mod int) int {
+	x := 1
+	y := 1
+	for i := 0; i < a; i++ {
+		x = x * (n - i)
+		x %= mod
+		y = y * (i + 1)
+		y %= mod
+	}
+	return x * modpow(y, mod-2, mod) % mod
+}
+
+func modfactorial(n, mod int) int {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result = (result * i) % mod
+	}
+	return result
+}
+
+// Utility
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func primeFactor(x int) map[int]int {
+	res := make(map[int]int)
+	for i := 2; i*i <= x; i++ {
+		for x%i == 0 {
+			res[i]++
+			x = x / i
+		}
+	}
+	if x != 1 {
+		res[x] = 1
+	}
+	return res
+}
+
+func divisor(x int) []int {
+	res := make([]int, 0)
+	for i := 1; i*i <= x; i++ {
+		if x%i == 0 {
+			res = append(res, i)
+			if i != x/i {
+				res = append(res, x/i)
+			}
+		}
+	}
+	return res
+}
+
+func gcd(x, y int) int {
+	if y == 0 {
+		return x
+	}
+	return gcd(y, x%y)
+}
+
+func lcm(x, y int) int {
+	return x / gcd(x, y) * y
+}
+
+func reverseString(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+// Queue ...
+type Queue []int
+
+// pop ...
+func (q *Queue) empty() bool {
+	return len(*q) == 0
+}
+
+// push ...
+func (q *Queue) push(i int) {
+	*q = append(*q, i)
+}
+
+// pop ...
+func (q *Queue) pop() (int, bool) {
+	if q.empty() {
+		return 0, false
+	} else {
+		res := (*q)[0]
+		*q = (*q)[1:]
+		return res, true
+	}
+}
+
+// Stack ...
+type Stack []int
+
+// pop ...
+func (s *Stack) empty() bool {
+	return len(*s) == 0
+}
+
+// push ...
+func (s *Stack) push(i int) {
+	*s = append(*s, i)
+}
+
+// pop ...
+func (s *Stack) pop() (int, bool) {
+	if s.empty() {
+		return 0, false
+	} else {
+		index := len(*s) - 1
+		res := (*s)[index]
+		*s = (*s)[:index]
+		return res, true
+	}
+}
