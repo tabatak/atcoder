e32683dc10d305add8b55a593f45153bf32e0b60
tabatak <hamamoto.kazuto@gmail.com>
2020-04-20 08:04:33 +0900

    ABC053-D (自力AC)
    priority_queueを使ったけど、解説を読むと不要だった。。

---
+++ abc053/d/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Fscan(r, &n)
+	var a int
+	nums := make(map[int]int)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(r, &a)
+		nums[a]++
+	}
+
+	// priority_queue
+	ih := &intHeap{}
+	heap.Init(ih)
+	for _, v := range nums {
+		if v >= 2 {
+			heap.Push(ih, v)
+		}
+	}
+
+	ans := n
+	for ih.Len() > 0 {
+		v1 := heap.Pop(ih)
+
+		var v2 interface{}
+		if ih.Len() > 0 {
+			v2 = heap.Pop(ih)
+		}
+
+		if v1.(int) > 2 {
+			heap.Push(ih, v1.(int)-1)
+		}
+		if v2 != nil && v2.(int) > 2 {
+			heap.Push(ih, v2.(int)-1)
+		}
+
+		ans -= 2
+	}
+
+	if ans <= 1 {
+		fmt.Println(1)
+	} else {
+		fmt.Println(ans)
+	}
+}
+
+type intHeap []int
+
+func (h intHeap) Len() int           { return len(h) }
+func (h intHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h intHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *intHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+func (h *intHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
