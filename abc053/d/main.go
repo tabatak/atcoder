d345f8691d03e5f16d1f83e4b5873df7fd57a9fe
tabatak <hamamoto.kazuto@gmail.com>
2020-04-20 08:07:50 +0900

    ABC053-D (解説を読んで修正)
    カードの種類に着目すればいいのか。。

--- abc053/d/main.go
+++ abc053/d/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"container/heap"
 	"fmt"
 	"os"
 )
@@ -18,40 +17,60 @@ func main() {
 		nums[a]++
 	}
 
-	// priority_queue
-	ih := &intHeap{}
-	heap.Init(ih)
-	for _, v := range nums {
-		if v >= 2 {
-			heap.Push(ih, v)
-		}
-	}
+	k := len(nums)
 
-	ans := n
-	for ih.Len() > 0 {
-		v1 := heap.Pop(ih)
-
-		var v2 interface{}
-		if ih.Len() > 0 {
-			v2 = heap.Pop(ih)
-		}
-
-		if v1.(int) > 2 {
-			heap.Push(ih, v1.(int)-1)
-		}
-		if v2 != nil && v2.(int) > 2 {
-			heap.Push(ih, v2.(int)-1)
-		}
-
-		ans -= 2
-	}
-
-	if ans <= 1 {
-		fmt.Println(1)
+	if (n-k)%2 == 0 {
+		fmt.Println(k)
 	} else {
-		fmt.Println(ans)
+		fmt.Println(k - 1)
 	}
 }
+
+// func main() {
+// 	r := bufio.NewReader(os.Stdin)
+// 	var n int
+// 	fmt.Fscan(r, &n)
+// 	var a int
+// 	nums := make(map[int]int)
+// 	for i := 0; i < n; i++ {
+// 		fmt.Fscan(r, &a)
+// 		nums[a]++
+// 	}
+
+// 	// priority_queue
+// 	ih := &intHeap{}
+// 	heap.Init(ih)
+// 	for _, v := range nums {
+// 		if v >= 2 {
+// 			heap.Push(ih, v)
+// 		}
+// 	}
+
+// 	ans := n
+// 	for ih.Len() > 0 {
+// 		v1 := heap.Pop(ih)
+
+// 		var v2 interface{}
+// 		if ih.Len() > 0 {
+// 			v2 = heap.Pop(ih)
+// 		}
+
+// 		if v1.(int) > 2 {
+// 			heap.Push(ih, v1.(int)-1)
+// 		}
+// 		if v2 != nil && v2.(int) > 2 {
+// 			heap.Push(ih, v2.(int)-1)
+// 		}
+
+// 		ans -= 2
+// 	}
+
+// 	if ans <= 1 {
+// 		fmt.Println(1)
+// 	} else {
+// 		fmt.Println(ans)
+// 	}
+// }
 
 type intHeap []int
 
