4c320fca7e5d362a285f69c71439fa6042bc9301
tabatak <hamamoto.kazuto@gmail.com>
2020-04-22 08:21:49 +0900

    ABC163-D (解説AC)
    端数のみで作れるsumの個数を求めればいいことまでは分かった。sumの上限については気づいたけど、下限を求めて上限との間の値をすべて取るというところに至らなかった。combination求めてしまうと間に合わず。

---
+++ abc163/d/main.go
@@ -0,0 +1,180 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	r := bufio.NewReader(os.Stdin)
+	var n, k int
+	fmt.Fscan(r, &n)
+	fmt.Fscan(r, &k)
+
+	mod := 1000000007
+	ans := 0
+	for i := k; i <= n+1; i++ {
+		sumMin := sumOfNums(0, i-1)
+		sumMax := sumOfNums(n-i+1, n)
+		ans += sumMax - sumMin + 1
+		ans %= mod
+	}
+
+	fmt.Println(ans)
+}
+
+func sumOfNums(l, r int) int {
+	return (l + r) * (r - l + 1) / 2
+}
+
+// mod combination
+func modpow(a, n, mod int) int {
+	res := 1
+	for n > 0 {
+		if n%2 != 0 {
+			res = res * a % mod
+		}
+		a = a * a % mod
+		n = n / 2
+	}
+	return res
+}
+
+func modcomb(n, a, mod int) int {
+	x := 1
+	y := 1
+	for i := 0; i < a; i++ {
+		x = x * (n - i)
+		x %= mod
+		y = y * (i + 1)
+		y %= mod
+	}
+	return x * modpow(y, mod-2, mod) % mod
+}
+
+func modfactorial(n, mod int) int {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result = (result * i) % mod
+	}
+	return result
+}
+
+// Utility
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func primeFactor(x int) map[int]int {
+	res := make(map[int]int)
+	for i := 2; i*i <= x; i++ {
+		for x%i == 0 {
+			res[i]++
+			x = x / i
+		}
+	}
+	if x != 1 {
+		res[x] = 1
+	}
+	return res
+}
+
+func divisor(x int) []int {
+	res := make([]int, 0)
+	for i := 1; i*i <= x; i++ {
+		if x%i == 0 {
+			res = append(res, i)
+			if i != x/i {
+				res = append(res, x/i)
+			}
+		}
+	}
+	return res
+}
+
+func gcd(x, y int) int {
+	if y == 0 {
+		return x
+	}
+	return gcd(y, x%y)
+}
+
+func lcm(x, y int) int {
+	return x / gcd(x, y) * y
+}
+
+func reverseString(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
+// Queue ...
+type Queue []int
+
+// pop ...
+func (q *Queue) empty() bool {
+	return len(*q) == 0
+}
+
+// push ...
+func (q *Queue) push(i int) {
+	*q = append(*q, i)
+}
+
+// pop ...
+func (q *Queue) pop() (int, bool) {
+	if q.empty() {
+		return 0, false
+	} else {
+		res := (*q)[0]
+		*q = (*q)[1:]
+		return res, true
+	}
+}
+
+// Stack ...
+type Stack []int
+
+// pop ...
+func (s *Stack) empty() bool {
+	return len(*s) == 0
+}
+
+// push ...
+func (s *Stack) push(i int) {
+	*s = append(*s, i)
+}
+
+// pop ...
+func (s *Stack) pop() (int, bool) {
+	if s.empty() {
+		return 0, false
+	} else {
+		index := len(*s) - 1
+		res := (*s)[index]
+		*s = (*s)[:index]
+		return res, true
+	}
+}
